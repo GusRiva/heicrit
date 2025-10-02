78f3d5559f4e20c18067a6aaaa583a4231979839
gustavo <gusfer@gmail.com>
2025-10-02 16:25:29 +0200

    WIP

--- test_data/pipelines/create_synoptic_map.py
+++ test_data/pipelines/create_synoptic_map.py
@@ -1,12 +1,15 @@
-from heipy.synopse import create_synopse_graph
+from heipy.synopse import create_synopse_graphs
+import os
 
 import test_config
+import config
 
-sigla_mapping = test_config.mapping
+# sigla_mapping = test_config.mapping
+sigla_mapping = config.mapping
 
-create_synopse_graph(
-    # [f"texts/{x}" for x in os.listdir('texts') if 'tr' not in x], 
-    ["texts/test.xml", "texts/test2.xml", "texts/test3.xml"],
+create_synopse_graphs(
+    [f"texts/{x}" for x in os.listdir('texts') if 'tr' not in x and 'AH' in x], 
+    # ["texts/test.xml", "texts/test2.xml", "texts/test3.xml"],
                sigla_mapping,
                map_criterion= 'xml:id'
                )
